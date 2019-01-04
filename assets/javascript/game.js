1b7f0ecda00174cf1f19d016b1d774914366c489
UCLAShawn3G <uclashawn1@gmail.com>
2019-01-04 08:57:22 -0800

    initial code

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// Variable Declarations
+var numR = 0;
+var numW = 0;
+var phrLgth = 0;
+var word = "";
+var numChar = 0;
+var tvPhrase = ["Game of Thrones", "Outlander", "Power", "Empire", "Spartacus"];
+var rand = 0;
+
+// Functions
+function start(){
+    document.getElementById('gamePg').style.display = "block";
+}
+
+// phrase, count characters, read text, player guess, win, loss, 
